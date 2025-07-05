b34d21d93944aea3a01b0ec017047e966b89f33b
alvinthroughchrist <90349446+alvinhandro@users.noreply.github.com>
2025-07-05 09:09:58 +0300

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,7 @@
-// posts.js - backend logic placeholder
+
+// routes/post.js
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const verifyToken = require("../middleware/verifyToken");// posts.js - backend logic placeholder
