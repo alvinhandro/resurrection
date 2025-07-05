f53f6b24063b4bf9598b29d23758a2ca39039c58
alvinthroughchrist <90349446+alvinhandro@users.noreply.github.com>
2025-07-05 09:08:05 +0300

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,13 @@
-// Post.js - backend logic placeholder
+// Post.js// models/Post.js
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  caption: { type: String, trim: true },
+  imageUrl: { type: String },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+}, { timestamps: true });
+
+module.exports = mongoose.model("Post", PostSchema);
+- backend logic placeholder
